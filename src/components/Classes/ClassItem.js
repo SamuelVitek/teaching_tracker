687902fca8245d351c7016411a728b910a70cf43
Samuel Vitek <Samuel.Vitek@aimtecglobal.com>
2023-05-15 12:21:25 +0200

    Fixing date time

--- src/components/Classes/ClassItem.js
+++ src/components/Classes/ClassItem.js
@@ -1,13 +1,20 @@
 import React from 'react';
 
 const ClassItem = (props) => {
+    const options = {
+        year: "numeric",
+        month: "numeric",
+        day: "numeric",
+        hour: '2-digit',
+        minute: '2-digit',
+    }
 
     return (
         <div className='pt-5'>
             <div className="flex items-center gap-x-4 text-xs">
                 <a href={'/' + props.classItem.subject.toLowerCase()}
                    className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">Math</a>
-                <time className="text-gray-500">{props.classItem.date.toLocaleString('en-GB', { timeZone: 'CET' })}</time>
+                <time className="text-gray-500">{props.classItem.date.toLocaleString('cs-CZ', options)}</time>
             </div>
             <div className='pt-3 pb-5 px-3'>
                 <p>{props.classItem.topic}</p>
