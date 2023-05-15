6565cb79d32fb58847966ee92da9be290c52c75b
Samuel Vitek <Samuel.Vitek@aimtecglobal.com>
2023-05-15 02:53:30 +0200

    Setting App to the dashboard screen (I didn't get to learn Routing from the course yet)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Login from './components/Login/Login';
+import Dashboard from './components/Dashboard/Dashboard'
 
 function App() {
   return (
     <div className='h-full bg-white'>
-      <Login />
+      {/*<Login />*/}
+        <Dashboard />
     </div>
   );
 }
